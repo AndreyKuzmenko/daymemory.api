6594e61e685769d0c392f1990f6a9e026fe5b7ab
Andrii Kuzmenko <a.kuzmenko2007@gmail.com>
2022-11-19 13:35:10 +0100

    mark tags as deleted

--- DayMemory.Core/CommandHandlers/Tags/DeleteTagByIdCommandHandler.cs
+++ DayMemory.Core/CommandHandlers/Tags/DeleteTagByIdCommandHandler.cs
@@ -1,21 +1,27 @@
 ﻿using DayMemory.Core.Commands;
 using DayMemory.Core.Interfaces.Repositories;
 using MediatR;
+using Microsoft.Extensions.Internal;
 
 namespace DayMemory.Core.CommandHandlers
 {
     public class DeleteTagByIdCommandHandler : IRequestHandler<DeleteTagCommand, Unit>
     {
         private readonly ITagRepository _tagRepository;
+        private readonly ISystemClock _clock;
 
-        public DeleteTagByIdCommandHandler(ITagRepository tagRepository)
+        public DeleteTagByIdCommandHandler(ITagRepository tagRepository, ISystemClock clock)
         {
             _tagRepository = tagRepository;
+            _clock = clock;
         }
 
         public async Task<Unit> Handle(DeleteTagCommand request, CancellationToken cancellationToken)
         {
-            await _tagRepository.DeleteByIdAsync(request.TagId!, cancellationToken);
+            var item = await _tagRepository.LoadByIdAsync(request.TagId!, cancellationToken);
+            item.IsDeleted = true;
+            item.ModifiedDate = _clock.UtcNow;
+            await _tagRepository.UpdateAsync(item, cancellationToken);
             return Unit.Value;
         }
     }
