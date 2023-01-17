f18e4d98dcbf791b111a1806db821651a1a51ad7
Andrii Kuzmenko <a.kuzmenko2007@gmail.com>
2023-01-17 19:53:54 +0100

    Test of video converting

--- DayMemory.VideoConverter/Function1.cs
+++ DayMemory.VideoConverter/Function1.cs
@@ -12,6 +12,7 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Xabe.FFmpeg;
 using Xabe.FFmpeg.Downloader;
+using Microsoft.Extensions.Configuration;
 
 namespace DayMemory.VideoConverter
 {
@@ -20,8 +21,25 @@ namespace DayMemory.VideoConverter
         [FunctionName("ConvertVideo")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log, ExecutionContext context)
         {
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+
+
+
+            var storageConnectionString = config["Values:AzureWebJobsStorage"];
+
+            if (string.IsNullOrEmpty(storageConnectionString))
+            {
+                storageConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            }
+
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
@@ -44,23 +62,51 @@ namespace DayMemory.VideoConverter
             FFmpeg.SetExecutablesPath(exeFolder);
             await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, exeFolder);
 
-            
 
-            //var containerName = "note-images";
-            //var blobContainerClient = new BlobContainerClient("", containerName);
+            var inputFilePath = Path.Combine(inputFileFolder, $"input.mov");
+            if (File.Exists(inputFilePath))
+            {
+                File.Delete(inputFilePath);
+            }
 
-            //BlobClient blob = blobContainerClient.GetBlobClient("a77f5313-1824-4b4d-ad54-b8b9f39af3e1/111111111");
-            //using (var stream = new MemoryStream())
-            //{
-            //    var b = await blob.DownloadToAsync(stream);
-            //    if (!b.IsError)
-            //    {
+            var outputFilePath = Path.Combine(outputFileFolder, $"output.mp4");
+            if (File.Exists(outputFilePath))
+            {
+                File.Delete(outputFilePath);
+            }
 
-            //    }
-            //}
+            var containerName = "note-images";
+            var blobContainerClient = new BlobContainerClient(storageConnectionString, containerName);
 
-            //await blob.UploadAsync(stream, new BlobUploadOptions() { HttpHeaders = new BlobHttpHeaders() { ContentType = contentType } });
+            BlobClient blob = blobContainerClient.GetBlobClient("a77f5313-1824-4b4d-ad54-b8b9f39af3e1/111111111");
+            using (var fileStream = System.IO.File.Create(inputFilePath))
+            {
+                var b = await blob.DownloadToAsync(fileStream);
+                if (!b.IsError)
+                {
 
+                    //using (var fileStream = System.IO.File.Create(inputFileFolder))
+                    //{
+                    //    stream.Seek(0, SeekOrigin.Begin);
+                    //    await stream.CopyToAsync(fileStream);
+                    //}
+
+
+
+
+
+                }
+            }
+
+
+            IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(inputFilePath);
+            var c = await FFmpeg.Conversions.FromSnippet.ToMp4(inputFilePath, outputFilePath);
+            await c.Start();
+
+            using (var fileStream = System.IO.File.OpenRead(outputFilePath))
+            {
+                //await blob.UploadAsync(stream, new BlobUploadOptions() { HttpHeaders = new BlobHttpHeaders() { ContentType = contentType } });
+            }
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
