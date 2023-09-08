0d8ebde1c5edb109cad495a17bbc550d0e685a76
Andrii Kuzmenko <a.kuzmenko2007@gmail.com>
2023-09-08 11:31:14 +0200

    database migration for tag encryption

---
+++ DayMemory.DAL/Migrations/20230908093040_TagIsEncrypted.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DayMemory.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagIsEncrypted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEncrypted",
+                table: "Tag",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEncrypted",
+                table: "Tag");
+        }
+    }
+}
